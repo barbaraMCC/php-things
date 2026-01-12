359caa4d65efadaf85c0c62767f0b91272e3e830
Barbara <barbaramorochuico@gmail.com>
2026-01-12 13:24:29 +0900

    forgot a file again

---
+++ loginUser.php
@@ -0,0 +1,56 @@
+<?php
+
+ session_start();
+
+ $servername = "localhost";
+ $username = "root";
+ $password = "";
+ $dbname = "lab_equipment";
+
+ // Create connection
+ $conn = new mysqli($servername, $username, $password, $dbname);
+
+ // Check connection
+ if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+ } else {
+    if ($_SERVER["REQUEST_METHOD"] == "POST") {
+        $email = trim($_POST['email']);
+        $password = trim($_POST['password']);
+    }
+
+    // Prepare SQL statement
+    $stmt = $conn->prepare("SELECT user_id,password FROM user WHERE email = ?");
+    $stmt->bind_param("s", $email);
+    $stmt->execute();
+    $result = $stmt->get_result();
+
+     if ($result->num_rows === 1) {
+        $row = $result->fetch_assoc();
+
+        if ($row['password'] === $password) {
+            // Login success
+            $_SESSION['user_id'] = $row['user_id'];
+            header("Location: index.php");
+            exit;
+        } else {
+            // wrong password
+            header("Location: login.php?error=wrongpass");
+            exit;
+        } 
+        
+    } else {
+        // user not found
+        header("Location: login.php?error=notfound");
+        exit;
+
+    }
+
+        $stmt->close();
+
+    }
+ 
+
+ $conn->close();
+
+?>
